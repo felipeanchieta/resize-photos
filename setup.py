56fd1dc0d66a81e82bb5337665b9b54fdd2d1aa6
Felipe Anchieta Santos Costa <felipeanchieta2@gmail.com>
2017-10-23 14:53:17 -0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='Resize Photos Challenge',
+      version='1.0',
+      description='Solution for the Resize Photos challenge',
+      author='Felipe Anchieta Santos Costa',
+      url='http://github.com/felipeanchieta',
+      packages=['resizephotos'],
+      )
