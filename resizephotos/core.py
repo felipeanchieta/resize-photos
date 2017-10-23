55edc3508adf6376c05d399303ad7e5c9c39f094
Felipe Anchieta Santos Costa <felipeanchieta2@gmail.com>
2017-10-23 20:18:39 -0200

    Major refactor

--- resizephotos/core.py
+++ resizephotos/core.py
@@ -1,92 +1,78 @@
 import json
-import os
 import re
-from io import StringIO
-from typing import List
+from io import BytesIO, StringIO
+from typing import List, Any, Tuple, Generator
 
 import requests
+import rfc6266 as rfc6266
 from PIL import Image
 from flask import Flask, jsonify, send_file
 from gridfs import GridFS
 from pymongo import MongoClient
-from scipy.misc import imresize, imread, imsave
 
 IMGS_URL = 'http://54.152.221.29/images.json'
+IMGS_SIZE = {"small": (240, 320), "medium": (288, 384), "large": (480, 640)}
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
 MONGO_DB = 'test'
 
-client = MongoClient(MONGO_HOST, MONGO_PORT)
-grid = GridFS(client[MONGO_DB])
+MultipleSizesImage = Tuple[type(Image), type(Image), type(Image)]
+
+_client = MongoClient(MONGO_HOST, MONGO_PORT)
+_db = _client[MONGO_DB]
+_client.drop_database(_db)
+grid = GridFS(_client[MONGO_DB])
 
 app = Flask(__name__)
 app.debug = True
 
 
-def download_originals(path: str = '.') -> List[str]:
+def download_originals() -> Generator:
     """
     Download the original images from IMGS_URL
 
     Return: the total of downloaded images.
     """
     r = requests.get(IMGS_URL)
-    images = json.loads(r.text)["images"]
-    files = []
+    j = json.loads(r.text)
 
-    for image in images:
-        url = image['url']
-        r = requests.get(url, stream=True)
-        filename = re.findall(r'.*/(.+)$', r.url)[0]
-        with open('{}'.format(path + '/' + filename), 'wb') as f:
-            for chunk in r:
-                f.write(chunk)
-        files.append(filename)
-
-    return files
+    for img in j['images']:
+        r = requests.get(img['url'])
+        stream = BytesIO(r.content)
+        img = Image.open(stream)
+        img.filename = rfc6266.parse_requests_response(r).filename_unsafe
+        yield img
 
 
-def list_images(path: str = '.') -> List[str]:
-    """
-    List the JPG images of a given directory. Default: pwd.
+def resize_image(img: type(Image)) -> Tuple[Any, Any, Any]:
+    """Resize an image to small, medium and large.
 
-    Return: list with the JPG filenames of given directory.
-    """
-    return [f for f in os.listdir(path) if f.endswith('.jpg') or f.endswith('.jpeg')]
+    Return: a tuple containing a small, medium, large image considering the original input."""
+    s_image = img.resize(IMGS_SIZE["small"])  # type: type(Image)
+    s_image.filename = "small_" + img.filename
+    m_image = img.resize(IMGS_SIZE["medium"])  # type: type(Image)
+    m_image.filename = "medium_" + img.filename
+    l_image = img.resize(IMGS_SIZE["large"])  # type: type(Image)
+    l_image.filename = "large_" + img.filename
 
-
-def resize_images(images: List[str]) -> int:
-    """
-    Resize the images of given directory, removing the original ones after that.
-
-    Return: the total number of resized images.
-    """
-
-    for filename in images:
-        image = imread(filename)
-        imsave('{name}_small.jpg'.format(name=filename.split('.')[0]), imresize(image, (240, 320)))
-        imsave('{name}_medium.jpg'.format(name=filename.split('.')[0]), imresize(image, (288, 384)))
-        imsave('{name}_large.jpg'.format(name=filename.split('.')[0]), imresize(image, (480, 640)))
-        os.remove(filename)
-
-    return len(images)
+    return s_image, m_image, l_image
 
 
-def persist_images(path: str = '.') -> None:
+def persist_image(img: type(Image), size: str = 'medium') -> bool:
     """Persist the JPG images of given path."""
-    for image in list_images(path):
-        with open(path + '/' + image, 'rb') as f:
-            grid.put(f, contentType='image/jpeg', filename=image)
+    grid.put(img.tobytes(), contentType='image/jpeg', filename=img.filename)
+    return True
 
 
 @app.route('/')
 def list_images_json():
     json_map = {}
     for image in grid.list():
-        if re.match(r'.+small_\.jpe?g$', image):
+        if re.match(r'^small_.*', image):
             json_map[image] = {"size": "small", "url": "/images/{}".format(image)}
-        elif re.match(r'.+medium\.jpe?g$', image):
+        elif re.match(r'^medium_.*', image):
             json_map[image] = {"size": "medium", "url": "/images/{}".format(image)}
-        elif re.match(r'.+large\.jpe?g$', image):
+        elif re.match(r'^large_.*', image):
             json_map[image] = {"size": "large", "url": "/images/{}".format(image)}
         else:
             json_map[image] = {"size": "unknown", "url": "/images/{}".format(image)}
@@ -97,7 +83,7 @@ def list_images_json():
 @app.route('/images/<filename>')
 def get_image(filename):
     if not grid.exists(filename=filename):
-        raise Exception("This image is not here, try somewhere else!")
+        return "<h1>Not Found</h1>", 404
     else:
         stream = grid.get_last_version(filename)
         image = Image.open(stream)
@@ -107,7 +93,20 @@ def get_image(filename):
         return send_file(stream, mimetype='image/jpeg')
 
 
+@app.before_first_request
+def initialize() -> None:
+    imgs = list(download_originals())  # type: List[type(Image)]
+    resized_imgs = [resize_image(img) for img in imgs]  # type: List[MultipleSizesImage]
+    for small, medium, large in resized_imgs:
+        persist_image(small, size='small')
+        persist_image(medium, size='medium')
+        persist_image(large, size='large')
+
+
+@app.errorhandler(404)
+def page_not_found(_: Any) -> Tuple[str, int]:
+    return "<h1>Not Found</h1>", 404
+
+
 if __name__ == '__main__':
-    images = download_originals()
-    resize_images(list_images())
     app.run()
--- tests/test_automated_tests.py
+++ tests/test_automated_tests.py
@@ -1,22 +1,72 @@
-import tempfile
+import json
 import unittest
+from typing import Sequence, List, Any, Tuple
 
-import os
+import requests
+from PIL import Image
 
 from resizephotos import core
 
+MultipleSizesImage = Tuple[type(Image), type(Image), type(Image)]
 
-class FlaskrTestCase(unittest.TestCase):
+
+class SanityCheckTest(unittest.TestCase):
     def setUp(self):
-        self.db_fd, core.app.config['DATABASE'] = tempfile.mkstemp()
-        core.app.testing = True
-        self.app = core.app.test_client()
-        with core.app.app_context():
-            core.app.init_db()
+        self.sanity_check()
 
-    def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(core.app.config['DATABASE'])
+    def assertNotEmpty(self, obj: Sequence) -> None:
+        """Asserts that a Sequence is not empty."""
+        self.assertGreater(len(obj), 0)
+
+    def sanity_check(self):
+        """Asserts the ideal conditions for the remote image server."""
+        r = requests.get('http://54.152.221.29/images.json')  # type: requests.Response
+        self.assertEqual(r.status_code, 200)
+        self.assertIn('content-type', r.headers)
+        self.assertRegex(r.headers['content-type'], r'application/json')
+        self.assertNotEmpty(r.text)
+
+        j = json.loads(r.text)
+        self.assertNotEmpty(j)
+        self.assertIn("images", j)
+        r.close()
+
+        imgs = j["images"]  # type: List[Any]
+        for img in imgs:
+            self.assertNotEmpty(img)
+            self.assertIn("url", img)
+            self.assertNotEmpty(img["url"])
+            r = requests.get(img["url"])  # type: requests.Response
+            self.assertEqual(r.status_code, 200)
+            self.assertIn('content-type', r.headers)
+            self.assertRegex(r.headers['content-type'], r'image/jpeg')
+            r.close()
+
+    def test_whole_process(self):
+        """Test the whole process of downloading the originals,
+        resizing them and persisting in Mongo"""
+
+        # Download images
+        imgs = list(core.download_originals())  # type: List[type(Image)]
+        self.assertGreater(len(imgs), 0)
+
+        # Resize images
+        resized_imgs = [core.resize_image(img) for img in imgs]  # type: List[MultipleSizesImage]
+        for small, medium, large in resized_imgs:
+            self.assertTupleEqual((240, 320), small.size)
+            self.assertTupleEqual((288, 384), medium.size)
+            self.assertTupleEqual((480, 640), large.size)
+
+        # Persist images
+        for small, medium, large in resized_imgs:
+            self.assertTrue(core.persist_image(small, size='small'))
+            # self.assertTrue(core.grid.exists(small.filename))
+            self.assertTrue(core.persist_image(medium, size='medium'))
+            # self.assertTrue(core.grid.exists(medium.filename))
+            self.assertTrue(core.persist_image(large, size='large'))
+            # self.assertTrue(core.grid.exists(large.filename))
+
+        print(core.grid.list())
 
 
 if __name__ == '__main__':
